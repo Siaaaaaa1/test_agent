219338ae691841508aedabb2f48b95b01c7ca256
Siaaaaaa1 <1092725908@qq.com>
2025-12-25 16:07:30 +0800

    fix: Update import statement for LlmClient to correct module path

--- agentevolver/module/task_manager/strategies/api_driven/__init__.py
+++ agentevolver/module/task_manager/strategies/api_driven/__init__.py
@@ -11,7 +11,7 @@ from loguru import logger
 from agentevolver.module.task_manager.strategies import TaskExploreStrategy
 from agentevolver.schema.task import Task, TaskObjective
 from agentevolver.schema.trajectory import Trajectory
-from AgentEvolver.agentevolver.client.llm_client_backup import LlmClient
+from agentevolver.module.task_manager.base import LlmClient
 from agentevolver.utils.utils import extract_json_from_str
 
 # 引入 Prompt
