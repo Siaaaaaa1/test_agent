1e68580399a0d9f2cd19b03d0313bc1579a4e214
Siaaaaaa1 <1092725908@qq.com>
2026-01-18 02:10:53 +0800

    feat: Configure Ray initialization with a specified number of CPUs

--- env_service/env_service.py
+++ env_service/env_service.py
@@ -140,7 +140,7 @@ class EnvService:
         )
 
         if not ray.is_initialized():
-            ray.init(address='local')
+            ray.init(address='local', num_cpus=32)
         self.env_actors = {}
         self.remote_env = {}
         self.last_access_time = {}
